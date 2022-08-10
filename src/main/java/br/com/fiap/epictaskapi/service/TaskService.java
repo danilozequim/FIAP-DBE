40785d5bfebe1f17fa0bb0cde350a3da29ca6591
Danilo <dzz@pop-os.localdomain>
2022-08-09 21:32:33 -0300

    first commit

---
+++ src/main/java/br/com/fiap/epictaskapi/service/TaskService.java
@@ -0,0 +1,24 @@
+package br.com.fiap.epictaskapi.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.epictaskapi.model.Task;
+import br.com.fiap.epictaskapi.repository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    TaskRepository repository;
+    
+    public List<Task> listAll(){
+        return repository.findAll();
+    }
+
+    public void save(Task task){
+        repository.save(task);
+    }
+}
